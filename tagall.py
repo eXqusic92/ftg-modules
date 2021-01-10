a9265a132e8786031c8f47b98c3dbb491f049219
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-01-11 01:25:04 +0200

    Update tagall.py

--- tagall.py
+++ tagall.py
@@ -2,6 +2,7 @@ import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantsBots
 from uniborg.util import admin_cmd
+from .. import utils
 
 
 @borg.on(admin_cmd("tagall"))
@@ -10,8 +11,9 @@ async def _(event):
         return
     mentions = ""
     counter = 0
+    args = utils.get_args(message)
     chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, 100):
+    async for x in borg.iter_participants(chat, args[0]):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
         counter += 1
         if counter == 1:
