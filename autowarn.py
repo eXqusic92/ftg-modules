004b14160bd73311c12296fe6c4eceaafb84b492
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-01-11 19:37:13 +0200

    test

--- autowarn.py
+++ autowarn.py
@@ -1,4 +1,12 @@
-.eval text = reply.text.split("=")
-r = text[2].split('"')
-id = int(r[0])
-return id
+# .eval text = reply.text.split("=")
+# r = text[2].split('"')
+# id = int(r[0])
+# return id
+
+import asyncio
+from telethon import events
+
+@client.on(events.NewMessage(pattern='(?i)hello.+'))
+async def handler(event):
+    # Respond whenever someone says "Hello" and something else
+    await event.reply('Hey!')
