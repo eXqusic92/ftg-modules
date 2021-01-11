2c998464fd751f20f7b43b8fbaebc09222cc216a
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-01-11 13:03:08 +0200

    Update tagall.py

--- tagall.py
+++ tagall.py
@@ -12,13 +12,25 @@ class TagallMod(loader.Module):
 
     async def tagallcmd(self, event):
         """Обычный тэг-модуль"""
+        global text
         try:
             mentions = ""
             counter = 0
             args = utils.get_args(event)
             chat = await event.get_input_chat()
-            async for x in event.client.iter_participants(chat, limit=int(args[0])):
-                mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">привет</a>"
+            if args > 1:
+                text = " ".join(args[1:])
+
+            if args:
+                count = int(args[0].strip())
+            else:
+                count = 20
+
+            async for x in event.client.iter_participants(chat, limit=count):
+                if text:
+                    mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">" + text + "</a>"
+                else:
+                    mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">" + x.first_name + "</a>"
                 counter += 1
                 if counter == 1:
                     await event.reply(mentions)
@@ -30,4 +42,4 @@ class TagallMod(loader.Module):
             await event.reply(mentions)
             await event.delete()
         except:
-            return await event.client.send_message(event.chat_id, '.tagall [количество юзеров(не больше 100)]')
+            return await event.client.send_message(event.chat_id, 'Ты еблан блять? Введи .tagall [количество юзеров(не больше 100), по дефолту 20]')
