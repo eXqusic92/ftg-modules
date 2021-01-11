96aeb1d488de612cf5798bf3eeac4fa6125071f5
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-01-11 12:25:36 +0200

    Update tagall.py

--- tagall.py
+++ tagall.py
@@ -1,27 +1,31 @@
-import asyncio
-from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantsBots
-from uniborg.util import admin_cmd
-from .. import utils
+from .. import loader, utils
+from asyncio import sleep, gather
 
 
-@borg.on(admin_cmd("tagall"))
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = ""
-    counter = 0
-    args = utils.get_args(message)
-    chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, args[0]):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-        counter += 1
-        if counter == 1:
-            await event.reply(mentions)
-            counter = 0
+def register(cb):
+    cb(TagallMod())
+
+class TagallMod(loader.Module):
+    """Tag Module for Bar of Don Salieri"""
+    strings = {'name': 'Spam'}
+
+    async def tagallcmd(self, message):
+        """Обычный тэг-модуль"""
+        try:
             mentions = ""
-    if counter == 0:
-        await event.delete()
-        return
-    await event.reply(mentions)
-    await event.delete()
+            counter = 0
+            args = utils.get_args(message)
+            chat = await message.get_input_chat()
+            async for x in client.iter_participants(chat, args[0]):
+                mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
+                counter += 1
+                if counter == 1:
+                    await message.reply(mentions)
+                    counter = 0
+                    mentions = ""
+            if counter == 0:
+                await message.delete()
+                return
+            await message.reply(mentions)
+            await message.delete()
+        except: return await message.client.send_message(message.to_id, '.tagall [количество юзеров(не больше 100)]')
