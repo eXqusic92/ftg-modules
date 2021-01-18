2a76fb5c7a08f4d951c333d87e0a8c5ca93f1e22
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-01-18 18:19:46 -0500

    fix

--- mutechat.py
+++ mutechat.py
@@ -12,17 +12,17 @@ class MuteAllMod(loader.Module):
         global turned
         if turned:
             turned = False
-            await message.edit("<b>Эти жалкие людишки снова могут говоритЬ!</b>")
+            await message.respond("<b>Эти жалкие людишки снова могут говоритЬ!</b>")
         else:
             turned = True
-            await message.edit("<b>Молчать всем!</b>")
+            await message.respond("<b>Молчать всем!</b>")
 
     async def watcher(self, message):
         """почему это называется watcher???"""
         global turned, excl
         if turned:
             from_id = message.from_id
-            if from_id in excl and message.chat_id == -1001430533627:
+            if from_id in excl:
                 pass
             else:
                 message.delete()
