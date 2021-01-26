2603a9de15467c133a601a1f07a0aa741a4b4c46
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-01-26 22:20:00 +0200

    fix

---
+++ autogift.py
@@ -0,0 +1,10 @@
+from .. import loader, utils
+
+class MuteAllMod(loader.Module):
+    """Автоподбор подарков из канала True Mafia"""
+    strings = {'name': 'AutoGift'}
+
+    async def watcher(self, event):
+        """почему это называется watcher???"""
+        if 'подарков' in event.raw_text.split():
+            event.click()
