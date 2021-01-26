3e1155dc1bddd63e8c3373f74b023a13ee02404f
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-01-26 21:31:40 +0200

    fix

--- autowarn.py
+++ autowarn.py
@@ -18,6 +18,13 @@ class WelcomeMod(loader.Module):
         text = message.text.split("=")
         r = text[2].split('"')
         id = int(r[0])
+        userent = await message.event.get_entity(id)
+        if userent.last_name == None:
+            username = str(userent.first_name)
+        elif userent.last_name and userent.first_name:
+            username = str(userent.first_name) + " " + str(userent.last_name)
+        else:
+            username = str(userent.last_name)
         chatid = message.chat_id
         msgtxt = str(message.text)
         fromid = message.from_id
@@ -26,32 +33,32 @@ class WelcomeMod(loader.Module):
             if 'бу-у-у-у-ду' in message.raw_text.split():
                 if id in admin_ids:
                     await message.reply("!Вот петушара, админ, ещё и АФКшит...")
-                    await message.client.send_message(1361873517, "Слыш ты бля " + "<a href=\"tg://user?id=" + str(id) + "\">" + "чурка ебаная" + "</a>" + " тебе Влад очко порвёт")
+                    await message.client.send_message(1361873517, "<a href=\"tg://user?id=" + str(id) + "\">" + username + "</a>" + " сидит в афк псина")
                 elif id in souch_ids:
                     await message.reply("!Уважаю")
-                    await message.client.send_message(1361873517, "Ебать " + "<a href=\"tg://user?id=" + str(id) + "\">" + "братишка" + "</a>" + ", будь поаккуратнее")
+                    await message.client.send_message(1361873517, "Респект и уважение этому человеку " + "<a href=\"tg://user?id=" + str(id) + "\">" + username + "</a>" +)
                 elif id == vlad_id:
                     await message.reply("!Блядь, я тебя захуярю")
                     await message.client.send_message(1361873517, "Блять" + "<a href=\"tg://user?id=" + str(id) + "\">" + "Влад" + "</a>" + " хули ты сам сидишь и дрочеш в группе, а на других орёшь что они афк? А?А?А?А?")
                 else:
                     await message.client.send_message(-1001430533627, "!warn " + str(id) + " AFK (Читать " + "<a href=\"https://t.me/rules_salieri/14\">Правила</a>)")
-                    await message.client.send_message(1361873517, "<b>[AFK/Warn] </b>Выдал варн " + "<a href=\"tg://user?id=" + str(id) + "\">" + "пиздюку" + "</a>" + " ибо нехуй сидеть в афк")
+                    await message.client.send_message(1361873517, "<b>[AFK/Warn] </b>Выдал варн " + "<a href=\"tg://user?id=" + str(id) + "\">" + username + "</a>" + " ибо нехуй сидеть в афк")
 
             if 'гнетущей' in message.raw_text.split():
                 str1 = message.text.split("\"")
                 id = int(str1[1].strip("tg://user?id="))
                 if id in admin_ids:
                     await message.reply("!Нахуй ливаешь, долбоёб?")
-                    await message.client.send_message(1361873517, "Ало " + "<a href=\"tg://user?id=" + str(id) + "\">" + "блядь" + "</a>" + " ты зашёл в катку чтобы повыпендриваться? Так сиди до конца, а не ливай посреди катки как крыса.")
+                    await message.client.send_message(1361873517, "Ало " + "<a href=\"tg://user?id=" + str(id) + "\">" + username + "</a>" + " ты зашёл в катку чтобы повыпендриваться? Так сиди до конца, а не ливай посреди катки как крыса.")
                 elif id in souch_ids:
                     await message.reply("!Ну и пошёл нахуй отсюдава")
-                    await message.client.send_message(1361873517, "Ммм.. Хорошая попытка сбежать, мой " + "<a href=\"tg://user?id=" + str(id) + "\">" + "друг" + "</a>")
+                    await message.client.send_message(1361873517, "Ммм.. Хорошая попытка сбежать, " + "<a href=\"tg://user?id=" + str(id) + "\">" + username + "</a>")
                 elif id == vlad_id:
                     await message.reply("!Влад зассал ибо против него играют слишком сильные противники")
                     await message.client.send_message(1361873517, "Krasav4ik - " + "<a href=\"tg://user?id=" + str(id) + "\">" + "Влад" + "</a>")
                 else:
                     await message.client.send_message(-1001430533627, "!warn " + str(id) + " Лив из игры (Читать " + "<a href=\"https://t.me/rules_salieri/14\">Правила</a>)")
-                    await message.client.send_message(1361873517, "<b>[Leave/Warn] </b>Выдал варн " + "<a href=\"tg://user?id=" + str(id) + "\">" + "пиздюку" + "</a>" + " ибо нехуй ливать с катки как последнее ссыкло")
+                    await message.client.send_message(1361873517, "<b>[Leave/Warn] </b>Выдал варн " + "<a href=\"tg://user?id=" + str(id) + "\">" + username + "</a>" + " ибо нехуй ливать с катки как последнее ссыкло")
 
         # except:
         #     pass
