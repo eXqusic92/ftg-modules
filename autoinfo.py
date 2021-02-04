5664c04861fde739143c3c7071eda7a142c92fda
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-02-04 20:34:51 +0200

    fix

---
+++ autoinfo.py
@@ -0,0 +1,11 @@
+from .. import loader, utils
+
+class AutoInfoMod(loader.Module):
+    """Автоинфо"""
+    strings = {'name': 'AutoInfo'}
+
+    async def watcher(self, event):
+        """почему это называется watcher???"""
+        if 'инфо' == event.raw_text.lower():
+            fromid = event.from_id
+            await event.respond(f'!info {fromid}')
