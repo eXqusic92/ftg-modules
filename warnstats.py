4c955e6a57f0c04a6f7d046b0130d098cfc495ff
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-02-06 22:35:47 +0200

    fix

---
+++ warnstats.py
@@ -0,0 +1,23 @@
+from .. import loader, utils
+
+
+class WarnStatsMod(loader.Module):
+    """Статистика варнов"""
+    strings = {'name': 'WarnStats'}
+
+    async def client_ready(self, client, db):
+        self._db = db
+
+    async def statscmd(self, event):
+        """.stats"""
+        afks = self._db.get("warns", "afk", 0)
+        leaves = self._db.get("warns", "leave", 0)
+
+        text = f"""
+<b>За сегодня было выдано варнов:</b>
+
+<i>За АФК - {afks}
+За лив - {leaves}</i>
+"""
+        self._db.set("warns", "afk", 0)
+        self._db.set("warns", "leave", 0)
