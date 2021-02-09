bb217e8d55a20815080f2d7cc760892f6e4ac8db
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-02-09 22:02:18 +0200

    fix

--- tagall.py
+++ tagall.py
@@ -10,47 +10,71 @@ class TagallMod(loader.Module):
     """Tag Module for Bar of Don Salieri"""
     strings = {'name': 'SalieriTag'}
 
-    async def tagallcmd(self, event):
+    async def client_ready(self, client, db):
+        self._db = db
+
+    async def updcmd(self, event):
+        """.upd"""
+        await event.respond("<b>Обновляем список админов</b>")
+
+        admins = []
+        async for user in event.client.iter_participants(event.chat_id, limit=50):
+            admins.append(user.id)
+        self._db.set("admins", "ids", admins)
+        await event.respond("<b>Список админов успешно обновлён!</b>")
+
+    async def watcher(self, event):
         """Тэг-модуль пидоров для Bar of Don Salieri"""
-        global text
-        try:
-            unreg = [1564155100, 595975466]
-            mentions = ""
-            counter = 0
-            args = utils.get_args(event)
-            chat = await event.get_input_chat()
-            args_len = len(args)
-            if int(args_len) > 1:
-                text = " ".join(args[1:])
-            else:
-                text = None
+        global text, count
+        mssg = event.raw_text.split().lower()
+        admin_ids = self._db.get("admins", "ids", None)
+        fromid = event.from_id
+        if (('калл' in mssg) or ('каллалл' in mssg) or ('каллал' in mssg) or ('каллактив' in mssg)) and (fromid in admin_ids):
+            try:
+                unreg = [1564155100, 595975466]
+                mentions = ""
+                counter = 0
+                chat = await event.get_input_chat()
 
-            if args:
-                count = int(args[0].strip())
-            else:
-                count = 20
+                if len(mssg) == 1:
+                    count = 100
+                    text = None
+                elif len(mssg) == 2:
+                    try:
+                        count = int(mssg[1])
+                        text = None
+                    except:
+                        count = 100
+                        text = str(mssg[1])
+                elif len(mssg) > 2:
+                    try:
+                        count = int(mssg[1])
+                        text = " ".join(mssg[1:])
+                    except:
+                        count = 100
+                        text = " ".join(mssg[1:])
 
-            async for x in event.client.iter_participants(chat, limit=count):
-                if x.id in unreg:
-                    continue
-                if text:
-                    mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">" + text + "</a>"
-                else:
-                    mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">" + x.first_name + "</a>"
-                counter += 1
-                if counter == 1:
-                    msg = await event.client.send_message(event.chat_id, mentions)
-                    await msg.delete()
-                    counter = 0
-                    mentions = ""
-            if counter == 0:
+                async for x in event.client.iter_participants(chat, limit=count, aggressive=True):
+                    if x.id in unreg:
+                        continue
+                    if text:
+                        mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">" + text + "</a>"
+                    else:
+                        mentions += "<a href=\"tg://user?id=" + str(x.id) + "\">" + x.first_name + "</a>"
+                    counter += 1
+                    if counter == 1:
+                        msg = await event.client.send_message(event.chat_id, mentions)
+                        await msg.delete()
+                        counter = 0
+                        mentions = ""
+                if counter == 0:
+                    await event.delete()
+                    await asyncio.sleep(0.2)
+                    await event.respond("анрег")
+                    return
+                await event.reply(mentions)
                 await event.delete()
+            except Exception as e:
+                await event.client.send_message(event.chat_id, f'Ты еблан блять? Введи .tagall [количество юзеров(не больше 100), по дефолту 20]\n\n{e}')
                 await asyncio.sleep(0.2)
                 await event.respond("анрег")
-                return
-            await event.reply(mentions)
-            await event.delete()
-        except Exception as e:
-            await event.client.send_message(event.chat_id, f'Ты еблан блять? Введи .tagall [количество юзеров(не больше 100), по дефолту 20]\n\n{e}')
-            await asyncio.sleep(0.2)
-            await event.respond("анрег")
