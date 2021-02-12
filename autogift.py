50f8343aea0e57ac2ba95cf4b1ae22cf44799248
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-02-12 22:16:21 +0200

    fix

--- autogift.py
+++ autogift.py
@@ -6,7 +6,7 @@ class MuteAllMod(loader.Module):
 
     async def watcher(self, event):
         """почему это называется watcher???"""
-        if 'несколько' in event.raw_text.split():
+        if ('несколько' in event.raw_text.split()) or ('поделился' in event.raw_text.split()):
             result = await event.click()
             try:
                 await event.client.send_message('me', result.message)
