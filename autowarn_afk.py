d3e17b97162b44d33af38aadd7e04c84bffcd4b2
eXqusic92 <pavlo.hreva.knm.2020@lpnu.ua>
2021-02-15 22:24:34 +0000

    fix

---
+++ autowarn_afk.py
@@ -0,0 +1,22 @@
+from .. import loader, utils
+from telethon.tl.types import ChannelParticipantsBanned
+
+class AutoWarnforMuteMod(loader.Module):
+    """AutoWarn for game with mute"""
+    strings = {'name': 'MuteWarn'}
+
+    async def watcher(self, message):
+        """watcher"""
+        muted = []
+        fromid = message.from_id
+        chatid = message.chat_id
+        msg = message.raw_text.split()
+        if (chatid == -1001430533627) and (fromid == 1044037207):
+            if '' in msg:
+                x = await client.get_participants(-1001430533627, limit=100, filter=ChannelParticipantsBanned)
+                for i in x:
+                    muted.append(i.id)
+                for usr in message.entities:
+                    if hasattr(usr, 'user_id'):
+                        if usr.user_id in muted:
+                            await message.client.send_message(-1001430533627,)
