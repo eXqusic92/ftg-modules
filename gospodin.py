25a7b438824ca5bacaa42d647f7b3a77e46aa59b
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-02-26 22:03:14 +0200

    new

---
+++ gospodin.py
@@ -0,0 +1,16 @@
+from .. import loader, utils
+
+known_id = [508169464, 1564155100]
+
+
+class ZazDeleteMod(loader.Module):
+    """Авто-удаление сообщений Зазывала tag-bot"""
+    strings = {'name': 'ZazDelete'}
+
+    async def watcher(self, message):
+        """Авто-удаление сообщений Зазывала tag-bot"""
+        fromid = message.from_id
+        if message.raw_text.lower() == "бот!" and fromid in known_id:
+            await message.reply("Приветствую, мой господин!🤝")
+        elif message.raw_text.lower == "бот!" and fromid not in known_id:
+            await message.reply("Ты че за ноунейм нах, отвали")
