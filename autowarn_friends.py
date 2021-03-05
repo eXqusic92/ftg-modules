4e8037e1530bf9e5053540e7dc2079f87f2f020d
eXqusic92 <pavlo.hreva.knm.2020@edu.lpnu.ua>
2021-03-05 22:15:23 +0200

    fix

--- autowarn_friends.py
+++ autowarn_friends.py
@@ -29,42 +29,44 @@ class WelcomeMod(loader.Module):
         pass_roles_1 = ["житель", "Счастливчик", "Санта", "Маньяк", "Самоубийца"]
         admin_ids = self._db.get("admins", "ids", None)
 
-        chatid = message.chat_id
+        # chatid = message.chat_id
         fromid = message.from_id
         sud_state = self._db.get("AutoWarn", "sud_state", False)
 
         if sud_state:
             return
-        if chatid == -1001170767846 and fromid == 761250017:
+        # chatid == -1001170767846 and
+        if fromid == 761250017:
             if ('убит' in message.raw_text.split()) and ("гостях" not in message.raw_text.split()):
                 if message.raw_text.split()[5] in pass_roles:
                     return
                 for usr in message.entities:
                     if hasattr(usr, 'user_id'):
                         uid = usr.user_id
-                        userent = await message.client.get_entity(uid)
-                        if userent.last_name is None:
-                            username = str(userent.first_name)
-                        elif userent.last_name and userent.first_name:
-                            username = str(userent.first_name) + " " + str(userent.last_name)
-                        else:
-                            username = str(userent.last_name)
+                        # userent = await message.client.get_entity(uid)
+                        # if userent.last_name is None:
+                        #     username = str(userent.first_name)
+                        # elif userent.last_name and userent.first_name:
+                        #     username = str(userent.first_name) + " " + str(userent.last_name)
+                        # else:
+                        #     username = str(userent.last_name)
 
                         if uid in admin_ids:
-                            await message.client.send_message(-1001177665247, f"Админ <a href=\"tg://user?id={str(uid)}\">{username}</a> сидит в афк\n\n")
+                            return
+                            # await message.client.send_message(-1001177665247, f"Админ <a href=\"tg://user?id={str(uid)}\">{username}</a> сидит в афк\n\n")
                         else:
                             await message.client.send_message(-1001170767846, f"!warn {str(uid)} AFK")
                             # await message.client.send_message(-1001177665247, f"<b>[AFK/Mute] </b>Выдал варн <a href=\"tg://user?id={str(uid)}\">{username}</a> за афк\n\n{timestamp}")
-                            cnt = self._db.get("warns", "afk", 0)
-                            self._db.set("warns", "afk", cnt+1)
-
-                            afk_list = self._db.get("afk", "warns")
-                            if username in afk_list:
-                                afk_list[username] = afk_list.get(username) + 1
-                                self._db.set("afk", "warns", afk_list)
-                            else:
-                                afk_list = {**afk_list, username: 1}
-                                self._db.set("afk", "warns", afk_list)
+                            # cnt = self._db.get("warns", "afk", 0)
+                            # self._db.set("warns", "afk", cnt+1)
+                            #
+                            # afk_list = self._db.get("afk", "warns")
+                            # if username in afk_list:
+                            #     afk_list[username] = afk_list.get(username) + 1
+                            #     self._db.set("afk", "warns", afk_list)
+                            # else:
+                            #     afk_list = {**afk_list, username: 1}
+                            #     self._db.set("afk", "warns", afk_list)
 
             if 'гнетущей' in message.raw_text.split():
                 msgs = []
@@ -78,26 +80,27 @@ class WelcomeMod(loader.Module):
                     if message.raw_text.split()[::-1][0] in pass_roles_1:
                         return
                     uid = message.entities[0].user_id
-                    userent = await message.client.get_entity(uid)
-                    if userent.last_name is None:
-                        username = str(userent.first_name)
-                    elif userent.last_name and userent.first_name:
-                        username = str(userent.first_name) + " " + str(userent.last_name)
-                    else:
-                        username = str(userent.last_name)
+                    # userent = await message.client.get_entity(uid)
+                    # if userent.last_name is None:
+                    #     username = str(userent.first_name)
+                    # elif userent.last_name and userent.first_name:
+                    #     username = str(userent.first_name) + " " + str(userent.last_name)
+                    # else:
+                    #     username = str(userent.last_name)
                     if uid in admin_ids:
-                        await message.client.send_message(-1001177665247, f"Админ <a href=\"tg://user?id={str(uid)}\">{username}</a> ливнул из катки\n\n")
+                        return
+                        # await message.client.send_message(-1001177665247, f"Админ <a href=\"tg://user?id={str(uid)}\">{username}</a> ливнул из катки\n\n")
                     else:
                         await message.client.send_message(-1001170767846, f"!warn {str(uid)} Лив из игры")
                         # await message.client.send_message(-1001177665247, f"<b>[Leave/Warn] </b>Выдал варн <a href=\"tg://user?id={str(uid)}\">{username}</a> за лив\n\n{timestamp}")
 
-                        cnt = self._db.get("warns", "leave", 0)
-                        self._db.set("warns", "leave", cnt + 1)
-
-                        leave_list = self._db.get("leave", "warns")
-                        if username in leave_list:
-                            leave_list[username] = leave_list.get(username) + 1
-                            self._db.set("leave", "warns", leave_list)
-                        else:
-                            leave_list = {**leave_list, username: 1}
-                            self._db.set("leave", "warns", leave_list)
+                        # cnt = self._db.get("warns", "leave", 0)
+                        # self._db.set("warns", "leave", cnt + 1)
+                        #
+                        # leave_list = self._db.get("leave", "warns")
+                        # if username in leave_list:
+                        #     leave_list[username] = leave_list.get(username) + 1
+                        #     self._db.set("leave", "warns", leave_list)
+                        # else:
+                        #     leave_list = {**leave_list, username: 1}
+                        #     self._db.set("leave", "warns", leave_list)
