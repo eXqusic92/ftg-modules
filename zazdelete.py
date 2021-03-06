e2c155456b6b88e4fc7bff1b81fc2ec8a62af71f
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-03-06 09:46:37 +0200

    fix

--- zazdelete.py
+++ zazdelete.py
@@ -1,19 +1,13 @@
 from .. import loader, utils
 
-zaz_id = [1263064901, 1169085389, 992657608, 803468136, 1362911415, 1302580284, 1355938374, 1383076852, 1283484635, 1181613391]
-
 
 class ZazDeleteMod(loader.Module):
     """Авто-удаление сообщений Зазывала tag-bot"""
-    strings = {'name': 'ZazDelete'}
+    strings = {'name': 'test1'}
 
     async def watcher(self, message):
-        """Авто-удаление сообщений Зазывала tag-bot"""
+        """1"""
         fromid = message.from_id
-        msg = message.raw_text.split()
-        if fromid in zaz_id:
-            if 'неактивности' not in msg:
-                if 'призывает' not in msg:
-                    if 'снова' not in msg:
-                        if len(msg) >= 2:
-                            await message.delete()
+        if fromid == 1611171382 and message.mentioned:
+            await message.delete()
+
