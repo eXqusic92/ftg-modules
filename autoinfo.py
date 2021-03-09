84ee12742a7c2277492ce3f5af18071bc6001b7d
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-03-09 12:45:17 +0200

    Update autoinfo.py

--- autoinfo.py
+++ autoinfo.py
@@ -9,13 +9,15 @@ logger = logging.getLogger(__name__)
 class AutoInfoMod(loader.Module):
     """Auto !info"""
     strings = {"name": "AutoInfo"}
-
+    
     async def client_ready(self, client, db):
         self._db = db
         self._me = await client.get_me()
 
+    async def switchcmd(self, message):
+        await message.respond("on")
+    
     async def watcher(self, message):
         if 'инфо' == message.raw_text.lower():
             fromid = message.from_id
-            await message.respond(f'!info {fromid}')
-
+            await utils.answer(message, f'!info {fromid}')
