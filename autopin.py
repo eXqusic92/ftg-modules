a6e3f3f63c58efe21136ffed39c24d47a4e6a6ee
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-04-11 00:02:47 +0300

    fix

--- autopin.py
+++ autopin.py
@@ -8,13 +8,13 @@ class AutoPinMod(loader.Module):
     async def watcher(self, message):
         """почему это называется watcher???"""
         baku_id = 1044037207
-        chatid = -1001430533627
-        if (message.from_id == baku_id) and (message.chat_id == chatid):
+        chatid = [-1001430533627, -1001170767846]
+        if (message.from_id == baku_id) and (message.chat_id in chatid):
             if 'набор' in message.raw_text.split():
                 await message.pin()
                 return
             if ('ветров' in message.raw_text.split()) or ('Недостаточно' in message.raw_text.split()):
-                x = await message.client.get_messages(-1001430533627, limit=30, from_user=baku_id)
+                x = await message.client.get_messages(message.chat_id, limit=30, from_user=baku_id)
                 for msg in x:
                     if 'набор' in msg.raw_text.split():
                         await msg.delete()
