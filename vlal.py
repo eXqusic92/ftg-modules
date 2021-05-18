a4f8866785708b6f87e7c825ad1d34bf9d59725e
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-05-18 20:01:00 +0300

    Update vlal.py

--- vlal.py
+++ vlal.py
@@ -12,5 +12,5 @@ class ZazDeleteMod(loader.Module):
     async def watcher(self, message):
         """1"""
         if message.raw_text.lower() == 'влал!':
-            m = [3, 25, 26]
+            m = [4, 25, 26]
             await utils.answer(message, await self._db.fetch_asset(random.choice(m)))
