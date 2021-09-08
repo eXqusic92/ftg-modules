f86b7d230830aca2fab3b5bba073b3554b7c92bb
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-09-08 23:31:54 +0300

    added showing chat

--- autogift.py
+++ autogift.py
@@ -8,7 +8,8 @@ class MuteAllMod(loader.Module):
         """почему это называется watcher???"""
         if ('несколько' in event.raw_text.split()) or ('поделился' in event.raw_text.split()):
             result = await event.click()
+            ent = await event.client.get_entity(event.sender_id)
             try:
-                await event.client.send_message('me', result.message)
+                await event.client.send_message('me', f"{result.message} - {ent} - {event.chat_id}")
             except:
                 pass
