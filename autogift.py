126f1e7af457d629ea343183fd3b48385475570b
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-09-09 11:23:30 +0300

    fix autogift

--- autogift.py
+++ autogift.py
@@ -7,9 +7,10 @@ class MuteAllMod(loader.Module):
     async def watcher(self, event):
         """почему это называется watcher???"""
         if ('несколько' in event.raw_text.split()) or ('поделился' in event.raw_text.split()):
+            if event.sender_id == 1303228016:
+                return
             result = await event.click()
-            ent = await event.client.get_entity(event.sender_id)
             try:
-                await event.client.send_message('me', f"{result.message} - {ent} - {event.chat_id}")
+                await event.client.send_message('me', f"{result.message}")
             except:
                 pass
